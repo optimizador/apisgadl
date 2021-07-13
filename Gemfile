688eadb10807b866c36349bd178a8ceb7ca1eaa0
pedro martinez <pbarrien@gmail.com>
2021-07-13 01:25:17 -0500

    update DL

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
-gem 'sinatra', '2.0.4'
 gem 'rerun',   '0.13.0'
 gem 'pg'
 gem 'sinatra-contrib'
 gem 'rest-client'
- 
--- server.rb
+++ server.rb
@@ -5,7 +5,7 @@ require "sinatra/namespace"
 require 'rest-client'
 
 set :bind, '0.0.0.0'
-set :port, 8080 
+set :port, 8080
 
 
 get '/' do
@@ -53,5 +53,39 @@ namespace '/api/v1' do
     end
     resultado.to_json
   end
+  get '/sizingdl' do
+    logger = Logger.new(STDOUT)
+    region="#{params['region']}"
+    type="#{params['type']}"
+    country_offer="#{params['country_offer']}"
+    puerto="#{params['puerto']}"
+    routing="#{params['rounting']}"
+    ha="#{params['ha']}"
+    resultado=[]
+    nodos=1
+    begin
+      if ha=="true" then
+        nodos=2
+      end
 
+      query="SELECT region, puerto, country_offer, type, price*#{nodos} as precio, routing FROM public.dl_pricing WHERE region='#{region}' and type='#{type}' and puerto='#{puerto}' and country_offer='#{country_offer}' and routing='#{routing}'";
+      logger.info(query)
+      connection = PG.connect :dbname => 'ibmclouddb', :host => '313a3aa9-6e5d-4e96-8447-7f2846317252.0135ec03d5bf43b196433793c98e8bd5.databases.appdomain.cloud',:user => 'ibm_cloud_31bf8a1b_1bbe_49e4_8dc2_0df605f5f88b', :port=>31184, :password => '535377ecca248285821949f6c71887d73a098f00b6908a645191503ab1d72fb3'
+      t_messages = connection.exec query
+      t_messages.each do |s_message|
+          resultado.push({ region: s_message['region'],
+            puerto: s_message['puerto'], country_offer: s_message['country_offer'],
+            type: s_message['type'],
+            precio: s_message['precio'],
+            nodos:  nodos,
+            routing: s_message['routing'] })
+      end
+      logger.info(resultado.to_s)
+    rescue PG::Error => e
+      logger.info(e.message.to_s)
+    ensure
+      connection.close if connection
+    end
+    resultado.to_json
+  end
 end
